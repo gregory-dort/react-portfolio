91db31f7916d1b75685615863124d33007be2204
Flebory <123499686+Flebory@users.noreply.github.com>
2025-11-17 10:31:27 -0500

    Added Resume Link to Hero Section

--- client/src/pages/Hero.tsx
+++ client/src/pages/Hero.tsx
@@ -51,39 +51,43 @@ const Hero = () => {
                 >
                    Connect with me here!
                 </motion.p>
-                <motion.div
-                    variants={itemVariants}
-                    className="flex flex-row space-x-4 justify-center"
-                >
-                    <motion.a
-                        href="https://github.com/gregory-dort"
-                        target="_blank"
-                        rel="noopener noreferrer"
+                <motion.div className="flex flex-col items-center mb-8">
+                    <motion.div
                         variants={itemVariants}
-                        className="text-gray-800 hover:text-cyan-300 hover:scale-115 transition duration-500"
+                        className="flex flex-row space-x-4 justify-center mb-4"
                     >
-                        <FaGithub size={30} />
-                    </motion.a>
-                    <motion.a
-                        href="https://www.linkedin.com/in/gregory-dort-776503218/"
-                        target="_blank"
-                        rel="noopener noreferrer"
+                        <motion.a
+                            href="https://github.com/gregory-dort"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="text-gray-800 hover:text-cyan-300 hover:scale-115 transition duration-500"
+                        >
+                            <FaGithub size={30} />
+                        </motion.a>
+                        <motion.a
+                            href="https://www.linkedin.com/in/gregory-dort-776503218/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="text-gray-800 hover:text-cyan-300 hover:scale-115 transition duration-500"
+                        >
+                            <FaLinkedin size={30} />
+                        </motion.a>
+                        <motion.a
+                            href="https://www.instagram.com/gdort07/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="text-gray-800 hover:text-cyan-300 hover:scale-115 transition duration-500"
+                        >
+                            <FaInstagram size={30} />
+                        </motion.a>
+                    </motion.div>
+                    <motion.button
                         variants={itemVariants}
-                        className="text-gray-800 hover:text-cyan-300 hover:scale-115 transition duration-500"
+                        whileHover={{ scale: 1.1 }}
+                        className="px-4 py-2 bg-cyan-100 border-1 border-gray-200 rounded-md shadow-md text-gray-800 font-serif hover:bg-cyan-300 transition duration-500"
+                        onClick={() => window.open('/Gregory Dort SWE Resume.pdf', '_blank')}
                     >
-                        <FaLinkedin size={30} />
-                    </motion.a>
-                    <motion.a
-                        href="https://www.instagram.com/gdort07/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        variants={itemVariants}
-                        className="text-gray-800 hover:text-cyan-300 hover:scale-115 transition duration-500"
-                    >
-                        <FaInstagram size={30} />
-                    </motion.a>
-                    <motion.button>
-                        View My Resume
+                            View My Resume
                     </motion.button>
                 </motion.div>
             </motion.div>
